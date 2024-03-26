686a0993e63aef871eaed68952b50f3818504f83
Hitesh Kuber <hiteshkuber@gmail.com>
2024-03-27 00:02:48 +0530

    Hello text quickstart

--- src/main/java/com/hitesh/quickstart/HelloWorldController.java
+++ src/main/java/com/hitesh/quickstart/HelloWorldController.java
@@ -1,4 +1,13 @@
 package com.hitesh.quickstart;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+
+    @GetMapping(path = "/hitesh")
+    public String basicPrint() {
+        return "Hitesh needs raise";
+    }
 }
